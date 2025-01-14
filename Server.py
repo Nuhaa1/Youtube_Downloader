6bec0c9dea09f6512b93aa208d4bf8bb13f5768c
Nuhaa1 <nuhaashahbani@gmail.com>
2025-01-15 04:23:24 +0800

    Fix port

--- Server.py
+++ Server.py
@@ -1,32 +1,13 @@
-from flask import Flask, send_file, Response
 import os
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 DOWNLOAD_PATH = "downloads/"
 
 @app.route('/downloads/<filename>')
 def download_file(filename):
-    file_path = os.path.join(DOWNLOAD_PATH, filename)
-    if os.path.exists(file_path):
-        return send_file(file_path, as_attachment=True)
-    else:
-        return "File not found", 404
-
-def generate_file(file_path):
-    with open(file_path, "rb") as f:
-        while chunk := f.read(8192):
-            yield chunk
-
-@app.route('/downloads/large/<filename>')
-def download_large_file(filename):
-    file_path = os.path.join(DOWNLOAD_PATH, filename)
-    if os.path.exists(file_path):
-        return Response(generate_file(file_path), headers={
-            'Content-Disposition': f'attachment; filename={filename}',
-            'Content-Type': 'application/octet-stream'
-        })
-    else:
-        return "File not found", 404
+    return send_from_directory(DOWNLOAD_PATH, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
