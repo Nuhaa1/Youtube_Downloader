a1066e940e2732279992a797ea581c877a6bafec
Nuhaa1 <nuhaashahbani@gmail.com>
2025-01-20 16:42:45 +0800

    Add files via upload

--- yt.py
+++ yt.py
@@ -404,9 +404,12 @@ def handle_tiktok_video(url, message):
             else:
                 logging.error(f"File not found: {unique_filepath}")
                 bot.send_message(chat_id, "Failed to download video. File not found after download.")
-    except Exception as e:
+    except yt_dlp.utils.ExtractorError as e:
         logging.error(f"Error during video processing: {e}", exc_info=True)
         bot.send_message(chat_id, "Failed to download video. Currently, TikTok downloads are unavailable due to regional restrictions. Our servers are located in the US, where TikTok has imposed stricter access controls. This means we're currently unable to download TikTok videos. We apologize for the inconvenience and appreciate your understanding.")
+    except Exception as e:
+        logging.error(f"Unexpected error during video processing: {e}", exc_info=True)
+        bot.send_message(chat_id, f"Failed to download video. Error: {e}")
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_quality_callback(call):
